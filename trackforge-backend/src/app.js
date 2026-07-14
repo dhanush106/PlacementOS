a394eae82505c4e855a3a86acfc1010e12e2ce68
dhanush106 <sarpoordhanush@gmail.com>
2026-07-14 17:29:17 +0530

    CORS CHANGED

--- trackforge-backend/src/app.js
+++ trackforge-backend/src/app.js
@@ -31,8 +31,8 @@ app.use('/uploads', express.static('uploads'));
 // CORS Configuration
 app.use(
   cors({
-    // origin: 'https://placement-os-seven.vercel.app' || 'http://localhost:5173',
-    origin: '*', // Allow all origins for development; change in production
+    origin: 'https://placement-os-rmrw.vercel.app' || 'http://localhost:5173',
+    // origin: '*', // Allow all origins for development; change in production
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
