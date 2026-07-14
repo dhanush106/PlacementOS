69075180b13cd3f8e40fd1861b048fc2fbde48c7
dhanush106 <sarpoordhanush@gmail.com>
2026-07-14 21:02:17 +0530

    Frontend api url fixed

--- trackforge-frontend/src/utils/api.js
+++ trackforge-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://placement-os-seven.vercel.app/' || 'http://localhost:5000/api',
+  baseURL: 'https://placement-os-seven.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
