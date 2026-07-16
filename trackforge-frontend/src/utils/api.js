3f55de425fe33e26e6fe2ea70ec5f31edd8bf22a
dhanush106 <sarpoordhanush@gmail.com>
2026-07-17 00:14:51 +0530

    Fixing the coneection-18

--- trackforge-frontend/src/utils/api.js
+++ trackforge-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://placement-os-fdyy.vercel.app/api/',
+  baseURL: 'https://placement-os-fdyy.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
