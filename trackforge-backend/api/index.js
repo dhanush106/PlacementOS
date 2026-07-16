bd832151646231e27822adc2d2f14579e7926f36
dhanush106 <sarpoordhanush@gmail.com>
2026-07-17 00:03:28 +0530

    Fixing the coneection-16

--- trackforge-backend/api/index.js
+++ trackforge-backend/api/index.js
@@ -2,6 +2,15 @@
 
 // export default handler;
 
-export default function handler(req, res) {
-  return res.status(200).send("Hello from Vercel");
+// export default function handler(req, res) {
+//   return res.status(200).send("Hello from Vercel");
+// }
+
+import app from "../src/app.js";
+import { ensureDatabaseConnection } from "../src/server.js";
+
+export default async function handler(req, res) {
+  await ensureDatabaseConnection();
+
+  return app(req, res);
 }
