20d119ecd710a37f0a7546f05a6c52137b6922a6
dhanush106 <sarpoordhanush@gmail.com>
2026-07-17 00:00:45 +0530

    Fixing the coneection-15

--- trackforge-backend/api/index.js
+++ trackforge-backend/api/index.js
@@ -1,3 +1,7 @@
-import handler from "../src/server.js";
+// import handler from "../src/server.js";
 
-export default handler;
+// export default handler;
+
+export default function handler(req, res) {
+  return res.status(200).send("Hello from Vercel");
+}
